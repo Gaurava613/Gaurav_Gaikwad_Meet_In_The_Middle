37517f633ae39e1215115f5f891267eadf4b2542
Gaurava613 <gauravgaikwad8112002@gmail.com>
2024-12-31 00:39:27 +0530

    app.js file updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import CreateMeet from './components/createMeet';
+import AllMeets from './components/AllMeetings/allMeets';
+import SignUp from './components/SignUP/signUp';
+import Login from './components/Login/login';
+import { BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-meet" element={<CreateMeet />} />
+        <Route path="/all-meets" element={<AllMeets />} />
+      </Routes>
+    </Router>
   );
 }
 
